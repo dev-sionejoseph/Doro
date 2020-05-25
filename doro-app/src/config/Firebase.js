f852c0cf2f4c2627caac2570ba07efcd64a88d51
Student_M9 <Student_M9@PS-DAL-M9.local>
2020-05-25 11:26:37 -0500

    removed firebase config from index html to resolve auth error

---
+++ doro-app/src/config/Firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase';
+
+
+      // Your web app's Firebase configuration
+      var firebaseConfig = {
+        apiKey: "AIzaSyAfj6xBmTfkpl0PoOJmbdw5o2EB0YEnCpw",
+        authDomain: "doro-c8995.firebaseapp.com",
+        databaseURL: "https://doro-c8995.firebaseio.com",
+        projectId: "doro-c8995",
+        storageBucket: "doro-c8995.appspot.com",
+        messagingSenderId: "1079076452696",
+        appId: "1:1079076452696:web:c7c194d99b367756930d2d",
+        measurementId: "G-GSFN09M7G8"
+      };
+      // Initialize Firebase
+      const fireBase = firebase.initializeApp(firebaseConfig);
+      export default fireBase;
+            
