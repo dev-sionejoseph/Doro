e5bd98c725d8a1f95c73ec02c2c213c372cd7c7b
Student_M9 <Student_M9@PS-DAL-M9.local>
2020-05-25 14:47:45 -0500

    imported navbar to home

--- doro-app/src/components/HomePage.js
+++ doro-app/src/components/HomePage.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import Timer from './Timer';
+import NavBar from './NavBar';
 
 export default class HomePage extends Component {
     render() {
         return (
             <div id="home-page-wrap">
+                <NavBar />
                 <Timer />
             </div>
         )
