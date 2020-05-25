07a5caa3198d55de63624f565770f12f2f47b687
Student_M9 <Student_M9@PS-DAL-M9.local>
2020-05-24 21:05:16 -0500

    auth basics to catch uuid and pass to db

--- doro-app/src/App.js
+++ doro-app/src/App.js
@@ -1,11 +1,46 @@
-import React from 'react';
+import firebase from 'firebase'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  constructor() {
+    super();
+    
+    this.state = ({
+      user: null
+    });
+
+    this.authcheck = this.authcheck.bind(this);
+  }
+
+  authcheck() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if(user) {
+        this.setState({ 
+          user: true 
+        });
+      } else {
+
+        this.setState({
+          user: null
+        });
+      }
+
+    });
+  }
+
+  componentDidMount() {
+    this.authcheck();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.user ? (<HomePage />) : (<SignInPage />)}
+      </div>
+    )
+  }
 }
 
-export default App;
+
