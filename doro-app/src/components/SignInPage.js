6b8214191d19c650c52a951cb6a612f0d014f9bb
Student_M9 <Student_M9@PS-DAL-M9.local>
2020-05-25 01:41:05 -0500

    updates to Sign In and Spotify Window

---
+++ doro-app/src/components/SignInPage.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+
+export default class SignInPage extends Component {
+    constructor(props){
+        super(props);
+
+        this.state({
+            signIn: true,
+            signUp: false
+        });
+    }
+    render() {
+        return (
+            <div id="sign-in-page-wrap">
+            {this.state.user ? (
+                <div id="sign-in-form">
+                    <div id="sign-in-input-wraps">
+                        <input className="sign-in-inputs" id="si-email" placeholder="Email Address"></input>
+                        <input className="sign-in-inputs" id="si-password" placeholder="Password"></input>
+                    </div>
+                    <div id="sign-in-button-wraps">
+                        <button className="sign-in-button">Sign In</button>
+                        <button className="sign-up-button">Sign Up</button>
+                    </div>
+                </div>
+                
+                ) : (
+                    <div id="sign-up-form">
+                        <div id="sign-up-input-wraps">
+                            <input className="sign-up-inputs" id="su-first-name" placeholder="First Name"></input>
+                            <input className="sign-up-inputs" id="su-last-name" placeholder="Last Name"></input>
+                            <input className="sign-up-inputs" id="su-email" placeholder="Email Address"></input>
+                            <input className="sign-up-inputs" id="su-password" placeholder="Password"></input>
+                        </div>
+                        <div id="sign-up-button-wraps">
+                            <button className="sign-in-button">Sign In</button>
+                            <button className="sign-up-button">Sign Up</button>
+                        </div>
+                    </div>
+                )}
+            
+            
+        </div>
+        )
+    }
+}
