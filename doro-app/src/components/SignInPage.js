a5540047b36ebc86034cccb70024f11327dc1c0a
Student_M9 <Student_M9@PS-DAL-M9.local>
2020-05-25 14:49:14 -0500

    updated handlers and buttons for sign up redirect

--- doro-app/src/components/SignInPage.js
+++ doro-app/src/components/SignInPage.js
@@ -20,7 +20,7 @@ export default class SignInPage extends Component {
         this.handleSignIn = this.handleSignIn.bind(this);
         this.handleSignUp = this.handleSignUp.bind(this);
         this.handleSubmit = this.handleChange.bind(this);
-        // this.addNoOpUser = this.addNoOpUser.bind(this);
+        this.addNoOpUser = this.addNoOpUser.bind(this);
 
     }
 
@@ -47,6 +47,32 @@ export default class SignInPage extends Component {
         });
     }
 
+    handleBack(){
+        this.setState({
+            signIn: true,
+            signUp: false
+        })
+    }
+    
+    async addNoOpUser(){
+            // post to database as noop user
+            let user = {
+                firstName: this.state.firstName,
+                lastName: this.state.lastName,
+                email: this.state.email,
+                id: this.state.id
+            }
+
+            await fetch('/doro/users/signup', {
+                method: 'POST',
+                headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(user),
+            });
+            }
+    
     handleSubmit(e){
 
         fireBase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password).then((u)=>{
@@ -62,9 +88,7 @@ export default class SignInPage extends Component {
       }
     
 
-    // addNoOpUser(){
-    //     // post to databasa as noop user
-    // }
+   
 
     render() {
         return (
@@ -72,26 +96,26 @@ export default class SignInPage extends Component {
             {this.state.signIn ? (
                 <div id="sign-in-form">
                     <div id="sign-in-input-wraps">
-                        <input className="sign-in-inputs" name="email" id="si-email" placeholder="Email Address"></input>
-                        <input className="sign-in-inputs" name="password" id="si-password" placeholder="Password"></input>
+                        <input className="sign-in-inputs" name="email" id="si-email" placeholder="Email Address" onChange={this.handleChange}></input>
+                        <input className="sign-in-inputs" name="password" id="si-password" placeholder="Password" onChange={this.handleChange}></input>
                     </div>
                     <div id="sign-in-button-wraps">
                         <button className="sign-in-button" onClick={this.handleSignIn}>Sign In</button>
-                        <button className="sign-up-button">Sign Up</button>
+                        <button className="sign-up-button" onClick={this.handleSignUp}>Sign Up</button>
                     </div>
                 </div>
                 
                 ) : (
                     <div id="sign-up-form">
                         <div id="sign-up-input-wraps">
-                            <input className="sign-up-inputs" name="firstName" id="su-first-name" placeholder="First Name"></input>
-                            <input className="sign-up-inputs" name="lastName" id="su-last-name" placeholder="Last Name"></input>
-                            <input className="sign-up-inputs" name="email" id="su-email" placeholder="Email Address"></input>
-                            <input className="sign-up-inputs" name="password" id="su-password" placeholder="Password"></input>
+                            <input className="sign-up-inputs" name="firstName" id="su-first-name" placeholder="First Name" onChange={this.handleChange}></input>
+                            <input className="sign-up-inputs" name="lastName" id="su-last-name" placeholder="Last Name" onChange={this.handleChange}></input>
+                            <input className="sign-up-inputs" name="email" id="su-email" placeholder="Email Address" onChange={this.handleChange}></input>
+                            <input className="sign-up-inputs" name="password" id="su-password" placeholder="Password" onChange={this.handleChange}></input>
                         </div>
                         <div id="sign-up-button-wraps">
-                            <button className="sign-in-button">Sign In Intead</button>
-                            <button className="submit-button">Submit</button>
+                            <button className="back-button" onClick={this.handleBack}>Back</button>
+                            <button className="submit-button"onClick={this.handleSubmit}>Submit</button>
                         </div>
                     </div>
                 )} 
