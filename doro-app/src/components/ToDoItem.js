1d2d4d758ea5ffbcf0535af01fb16bece5130d38
Student_M9 <Student_M9@PS-DAL-M9.local>
2020-05-25 21:19:04 -0500

    edits to item comp

--- doro-app/src/components/ToDoItem.js
+++ doro-app/src/components/ToDoItem.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class ToDoItem extends Component {
     render() {
         return (
-            <div className="to-do-item" id={this.props.id}>
+            <div className="to-do-item-wrap" id={this.props.id}>
                 <div className="item-edit">
                     <button className="delete" onClick={this.props.onClick}>x</button>
                     <button className="edit" onClick={this.props.onClick}>edit</button>
